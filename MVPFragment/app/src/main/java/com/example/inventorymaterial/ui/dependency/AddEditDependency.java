a1bd03df0087f2c846ea7d324bfd47c64a6dbb84
JuanFCrater <juanfbenitezlopez@gmail.com>
2017-11-24 14:45:42 +0100

    oh my gooddd

--- MVPFragment/app/src/main/java/com/example/inventorymaterial/ui/dependency/AddEditDependency.java
+++ MVPFragment/app/src/main/java/com/example/inventorymaterial/ui/dependency/AddEditDependency.java
@@ -4,19 +4,37 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.inventorymaterial.R;
+import com.example.inventorymaterial.ui.base.BaseFragment;
+import com.example.inventorymaterial.ui.base.BasePresenter;
 import com.example.inventorymaterial.ui.dependency.contrat.AddEditContrat;
+import com.example.inventorymaterial.ui.dependency.contrat.AddEditInteractor;
 
 /**
  * Created by usuario on 23/11/17.
  */
 
-public class AddEditDependency extends Fragment implements AddEditContrat.View {
+public class AddEditDependency extends BaseFragment implements AddEditContrat.View {
+
+
+    TextInputLayout tilname;
+    TextInputLayout tilshortname;
+    TextInputLayout tildescription;
+    EditText edtName;
     public static final String TAG="addeditdependency";
+    private AddEditContrat.Presenter presenter;
+
+
+    @Override
+    public void setPresenter(BasePresenter presenter) {
+    this.presenter= (AddEditContrat.Presenter) presenter;
+    }
     public static AddEditDependency newInstance(Bundle arguments) {
         AddEditDependency addEditDependency = new AddEditDependency();
         if(arguments!=null)
@@ -31,10 +49,15 @@ public class AddEditDependency extends Fragment implements AddEditContrat.View {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_add_edit_dependency,container,false);
         FloatingActionButton fab = rootView.findViewById(R.id.fab);
+        tilname = rootView.findViewById(R.id.tilName);
+        edtName= rootView.findViewById(R.id.edtName);
+
+        tilshortname = rootView.findViewById(R.id.tilShorname);
+        tildescription = rootView.findViewById(R.id.tilDescription);
         fab.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                presenter.validateDependency();
+                presenter.validatedependency(tilname.getEditText().getText().toString(),tilshortname.getEditText().getText().toString(),tilshortname.getEditText().getText().toString());
             }
         });
         if(getArguments()!=null)//es editar
@@ -42,5 +65,49 @@ public class AddEditDependency extends Fragment implements AddEditContrat.View {
 
         }
         return rootView;
+    }
+
+
+    @Override
+    public void showListDependency() {
+
+    }
+
+    @Override
+    public void showErrorName() {
+        tilname.setError(getResources().getString(R.string.errorNameEmpty));
+
+    }
+
+    @Override
+    public void showErrorShortName() {
+        tilshortname.setError(getResources().getString(R.string.errorShortNameEmpty));
+
+    }
+
+    @Override
+    public void showErrorDescription() {
+        tilshortname.setError(getResources().getString(R.string.errorDescriptionEmpty));
+
+    }
+
+    @Override
+    public void showDependencyExitsError() {
+        showMessage(getResources().getString(R.string.errorDependencyDuplicate));
+    }
+
+    @Override
+    public void setNameEmptyError() {
+    onError("NO name bitch");
+    }
+
+    @Override
+    public void setShortNameEmptyError() {
+        onError("NO shortname bitch");
+    }
+
+    @Override
+    public void setDescriptionEmptyError() {
+        onError("NO description bitch");
     }
 }
--- MVPFragment/app/src/main/java/com/example/inventorymaterial/ui/dependency/interactor/AddEditDependencyInteractorImpl.java
+++ MVPFragment/app/src/main/java/com/example/inventorymaterial/ui/dependency/interactor/AddEditDependencyInteractorImpl.java
@@ -1,8 +1,27 @@
 package com.example.inventorymaterial.ui.dependency.interactor;
 
+import android.text.TextUtils;
+
+import com.example.inventorymaterial.data.db.repository.DependencyRepository;
+import com.example.inventorymaterial.ui.dependency.contrat.AddEditInteractor;
+
 /**
  * Created by usuario on 23/11/17.
  */
 
 public class AddEditDependencyInteractorImpl {
+
+    public void validateDependency(String name, String shortname ,String description, AddEditInteractor.OnAddDependecyListener listener) {
+        //Si el password es vacío
+        if (TextUtils.isEmpty(name)) {
+            listener.onNameEmptyError();
+        } else if (TextUtils.isEmpty(shortname)) {
+            listener.onShortNameEmptyError();
+        } else if (TextUtils.isEmpty(description)) {
+            listener.onDescriptionError();
+        } else if(false)
+        {
+            listener.onDependencyDuplicated();
+        }
+    }
 }
