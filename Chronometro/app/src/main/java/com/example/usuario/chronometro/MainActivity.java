7a5ca487028c5f6b849acc159ec0a3122b15322f
JuanFCrater <juanfbenitezlopez@gmail.com>
2018-01-18 10:42:34 +0100

    Chrono

--- Chronometro/app/src/main/java/com/example/usuario/chronometro/MainActivity.java
+++ Chronometro/app/src/main/java/com/example/usuario/chronometro/MainActivity.java
@@ -6,9 +6,27 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    private ReverseChronometer chronometer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chronometer= findViewById(R.id.chrono);
+        chronometer.setOverallDuration(15);
+        chronometer.setWarningDuration(5);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        chronometer.run();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+       chronometer.stop();
+
     }
 }
--- Chronometro/app/src/main/java/com/example/usuario/chronometro/ReverseChronometer.java
+++ Chronometro/app/src/main/java/com/example/usuario/chronometro/ReverseChronometer.java
@@ -2,16 +2,66 @@ package com.example.usuario.chronometro;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
-
 @SuppressLint("AppCompatCustomView")
-public class ReverseChronometer extends TextView {
+public class ReverseChronometer extends TextView implements Runnable
+{
+    private long overallDuration,warningDuration,startTime;
 
     public ReverseChronometer(Context context, @Nullable AttributeSet attributes) {
         super(context,attributes);
+        TypedArray atribute=getContext().obtainStyledAttributes(attributes,R.styleable.ReverseChronometer);
+        overallDuration=atribute.getInteger(R.styleable.ReverseChronometer_overallduration,60);
+        warningDuration=atribute.getInteger(R.styleable.ReverseChronometer_warningduration,10);
+        setText("Valor inicial: "+overallDuration);
+        reset();
     }
 
+    public void setOverallDuration(long overallDuration)
+    {
+        this.overallDuration=overallDuration;
+    }
+    public void setWarningDuration(long warningDuration)
+    {
+        this.warningDuration=warningDuration;
+    }
+    public void reset()
+    {
+        startTime= SystemClock.elapsedRealtime();
+        setText("--:--");
+        setTextColor(Color.BLACK);
+    }
+    @Override
+    public void run(){
+            long elapseSeconds=(SystemClock.elapsedRealtime()-startTime)/1000;
+            if(elapseSeconds<overallDuration)
+            {
+                //Actualizar los tiempos
+                long remainingSecond=overallDuration-elapseSeconds;
+                long minutes=remainingSecond/60;
+                long seconds= remainingSecond%60;
+                setText(String.format("%d:%02d",minutes,seconds));
+
+                //En el caso que no encontremos en tiempo de warning
+                if(remainingSecond<warningDuration)
+                {
+                    setTextColor(Color.RED);
+                }
+
+                postDelayed(this,1000);
+            }else{
+                setText("00:00");
+                setTextColor(Color.BLACK);
+            }
+    }
+
+    public void stop() {
+        removeCallbacks(this);
+    }
 }
